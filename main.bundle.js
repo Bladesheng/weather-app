00c92a06b9a78d9c697bf40d3aa4fa0da7c39f82
Bladesheng <keadr23@gmail.com>
2022-08-21 16:36:02 +0200

    Add description

--- main.bundle.js
+++ main.bundle.js
@@ -87,7 +87,8 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
 var Cache = function () {
-  // module for xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  // module for storing and retrieving responses from cache,
+  // retrieves response for given url from cache, if it is invalid, fetches a new one
   function getResponse(_x) {
     return _getResponse.apply(this, arguments);
   } // returns false if response for given url is expired or nothing is not cached at all
