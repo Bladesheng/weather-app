400b3966235dff9e06b8b898b8b6aaa4bd2d0856
Bladesheng <keadr23@gmail.com>
2022-08-25 20:52:22 +0200

    Handle errors better in search function

--- src/index.ts
+++ src/index.ts
@@ -6,38 +6,46 @@ import { Geocode } from "./modules/Geocode";
 import { Storage } from "./modules/Storage";
 
 export async function search(searchValue: string) {
-  DOM.showLoader();
+  try {
+    DOM.showLoader();
 
-  DOM.wipeTabs();
+    DOM.wipeTabs();
 
-  cityHeading.textContent = searchValue;
+    cityHeading.textContent = searchValue;
 
-  // turn on / off favorite button if city is favorite
-  if (Storage.isFavorite(searchValue)) {
-    DOM.favoriteOn();
-  } else {
-    DOM.favoriteOff();
+    // turn on / off favorite button if city is favorite
+    if (Storage.isFavorite(searchValue)) {
+      DOM.favoriteOn();
+    } else {
+      DOM.favoriteOff();
+    }
+
+    const coords = await Geocode.getCoords(searchValue);
+
+    await Promise.all([
+      MET.getLocationforecast(coords),
+      MET.getSunrise(coords)
+    ]);
+
+    Weather.init();
+
+    DOM.createTodayTab();
+
+    // show next 9 days
+    for (let daysPast = 1; daysPast < 10; daysPast++) {
+      const date = new Date();
+      date.setDate(date.getDate() + daysPast);
+      DOM.createDayTab(date);
+    }
+
+    // cache searched city
+    Storage.lastCity = searchValue;
+
+    DOM.hideLoader();
+  } catch (error) {
+    console.log(error);
+    DOM.hideLoader();
   }
-
-  const coords = await Geocode.getCoords(searchValue);
-
-  await Promise.all([MET.getLocationforecast(coords), MET.getSunrise(coords)]);
-
-  Weather.init();
-
-  DOM.createTodayTab();
-
-  // show next 9 days
-  for (let daysPast = 1; daysPast < 10; daysPast++) {
-    const date = new Date();
-    date.setDate(date.getDate() + daysPast);
-    DOM.createDayTab(date);
-  }
-
-  // cache searched city
-  Storage.lastCity = searchValue;
-
-  DOM.hideLoader();
 }
 
 const searchBtn = document.querySelector("button.search");
